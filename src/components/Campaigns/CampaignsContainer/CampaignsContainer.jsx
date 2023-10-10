229b08e9c08a51d1a16a9010bdfce7958c6f51f3
Vivek Nepal <vivekn936@gmail.com>
2023-10-10 20:58:35 +0545

    CampaignData from CampaignContainer kept in separate file

--- src/components/Campaigns/CampaignsContainer/CampaignsContainer.jsx
+++ src/components/Campaigns/CampaignsContainer/CampaignsContainer.jsx
@@ -1,30 +1,7 @@
 import React from 'react'
 import styles from "./CampaignsContainer.module.scss"
 import CampaignsCard from '../CampaignsCard/CampaignsCard'
-
-const campaignData = [
-    {
-        "id": 1,
-        "redirectUrl": "https://viveknepal.com.np",
-        "imageSrc": "/campaign-img1.png",
-        "title": "Open Data Directive",
-        "desc": "In view of Russia's military action against the Ukrainian people, being concerned about ensuring that European funds In view of Russia's military action against the Ukrainian people,"
-    },
-    {
-        "id": 2,
-        "redirectUrl": "https://viveknepal.com.np",
-        "imageSrc": "/campaign-img2.png",
-        "title": "Data Security Standard",
-        "desc": "Lorem vero iusto minus ab magni repellat blanditiis cum in deserunt eos natus libero mollitia voluptates."
-    },
-    {
-        "id": 3,
-        "redirectUrl": "https://viveknepal.com.np",
-        "imageSrc": "/campaign-img3.png",
-        "title": "Data Privacy Levels",
-        "desc": "Lorem ipi enim qui. Cumque dolore illum consequuntur!"
-    },
-];
+import { campaignData } from './campaignData'
 
 const CampaignsContainer = () => {
   return (
