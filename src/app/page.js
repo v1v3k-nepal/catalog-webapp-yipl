555eafa63a28df43af43a8b3006392a77848241f
Vivek Nepal <vivekn936@gmail.com>
2024-05-21 11:31:52 +0545

    fix: removed clients section

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,26 @@
-import styles from './page.module.css'
-import CampaignsContainer from "@/components/Campaigns/CampaignsContainer/CampaignsContainer"
-import About from "@/components/WhoWeAre/About"
-import Clients from '@/components/Clients/Clients'
-import NewsContainer from '@/components/News/NewsContainer/NewsContainer'
-import ResContainer from '@/components/LatestResources/ResContainer/ResContainer'
-import Contact from '@/components/ContactUs/Contact'
-import Footer from '@/components/Footer/Footer'
-import Copyright from '@/components/Footer/Copyright'
-import Hero from '@/components/Hero/Hero'
+import styles from "./page.module.css";
+import CampaignsContainer from "@/components/Campaigns/CampaignsContainer/CampaignsContainer";
+import About from "@/components/WhoWeAre/About";
+import Clients from "@/components/Clients/Clients";
+import NewsContainer from "@/components/News/NewsContainer/NewsContainer";
+import ResContainer from "@/components/LatestResources/ResContainer/ResContainer";
+import Contact from "@/components/ContactUs/Contact";
+import Footer from "@/components/Footer/Footer";
+import Copyright from "@/components/Footer/Copyright";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
-      <Hero/>
-      <CampaignsContainer/>
-      <About/>
-      <Clients/>
-      <NewsContainer/>
-      <ResContainer/>
-      <Contact/>
-      <Footer/>
-      <Copyright/>
+      <Hero />
+      <CampaignsContainer />
+      <About />
+      {/* <Clients/> */}
+      <NewsContainer />
+      <ResContainer />
+      <Contact />
+      <Footer />
+      <Copyright />
     </div>
-  )
+  );
 }
